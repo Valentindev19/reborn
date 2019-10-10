6171269640575fec1c8c2b7c0b28682e022a1215
Valentin <valentindevaud@gmail.com>
2019-10-10 16:20:28 +0200

    srh

--- traitlog.php
+++ traitlog.php
@@ -10,8 +10,40 @@
       echo "GG";
     }
     else
-    {
-      echo "Cocher le captcha !!";
+    {?>
+      <html lang="fr" dir="ltr">
+      <link href="css/jquery-ui.css" rel="stylesheet">
+        <head>
+          <meta charset="utf-8">
+          <title>Page connexion</title>
+          <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        </head>
+        <body>
+          <link rel="stylesheet" href="css/log.css">
+          <div class="login-page">
+            <div class="form">
+              <form class="login-form" method="post" action="traitlog.php">
+                <input type="text" placeholder="mail"/>
+                <input type="password" placeholder="mot de passe"/>
+                <div class="g-recaptcha" data-sitekey="6Lca1qQUAAAAADWm9IN2V9Em3Vj-CmPdo3LG0U3j"></div>
+                <button>Connexion</button>
+                <p class="message">Pas inscrit ? <a href="conex.html">Créer un compte</a></p>
+              </form>
+
+            </div>
+          </div>
+          <div class="ui-widget">
+             <div class="ui-state-error ui-corner-all" style="padding: 0 .7em;">
+                 <p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>
+                     <strong>Erreur !</strong> Veuillez cocher le Captcha</p>
+                    </div>
+                  </div>
+        </body>
+      </html>
+      <script src="js/jquery.js"></script>
+      <script src="jquery-ui.js"></script>
+
+      <?php
       header('Refresh: 2; log.php');
     }
 
