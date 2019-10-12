11ecbc4144e6aa1fc3a278eea5344f5ea12258a4
Nekodow <56070023+Nekodow@users.noreply.github.com>
2019-10-12 18:51:53 +0200

    Le captcha me casse les couilles

--- log.php
+++ log.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
+<html lang="fr-fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Page connexion</title>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   </head>
   <body>
     <link rel="stylesheet" href="css/log.css">
@@ -12,12 +11,14 @@
         <form class="login-form" method="post" action="traitlog.php">
           <input type="text" placeholder="mail"/>
           <input type="password" placeholder="mot de passe"/>
-          <div class="g-recaptcha" data-sitekey="6Lca1qQUAAAAADWm9IN2V9Em3Vj-CmPdo3LG0U3j"></div>
+          <div class="g-recaptcha" style="position:relative; right:15px;" data-sitekey="6Lca1qQUAAAAADWm9IN2V9Em3Vj-CmPdo3LG0U3j"></div>
+          <br>
           <button>Connexion</button>
             <p class="message"> <a href="conex.html">Mot de passe oublié</a></p>
           <p class="message">Pas inscrit ? <a href="conex.html">Créer un compte</a></p>
         </form>
       </div>
     </div>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   </body>
 </html>
