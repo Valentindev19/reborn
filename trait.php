e83f35a790bd61eafe78d618173b217d2995b5d0
Nekodow <56070023+Nekodow@users.noreply.github.com>
2019-10-17 17:45:30 +0200

    ui

--- trait.php
+++ trait.php
@@ -1,17 +1,4 @@
 <?php
-  $nom;$prenom;$dn;$genre;$email;$phone;$cp;$vile;$adr;$comp;$mdp;$captcha;
-  $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
-  $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
-  $dn = filter_input(INPUT_POST, 'dn', FILTER_SANITIZE_STRING);
-  $genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_STRING);
-  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-  $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
-  $cp = filter_input(INPUT_POST, 'cp', FILTER_SANITIZE_STRING);
-  $ville = filter_input(INPUT_POST, 'ville', FILTER_SANITIZE_STRING);
-  $adr = filter_input(INPUT_POST, 'adr', FILTER_SANITIZE_STRING);
-  $comp = filter_input(INPUT_POST, 'comp', FILTER_SANITIZE_STRING);
-  $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_STRING);
-  $captcha = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
 
   if(!$captcha){
     echo '<h2>Please check the the captcha form.</h2>';
@@ -35,11 +22,38 @@
   $response = file_get_contents($url, false, $context);
   $responseKeys = json_decode($response,true);
   header('Content-type: application/json');
+  if ($responseKeys["success"])
+  {
+    if (isset($_POST["form_conex"])) {
+      $nom;$prenom;$dn;$genre;$email;$phone;$cp;$vile;$adr;$comp;$mdp;$captcha;
+      $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+      $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
+      $dn = filter_input(INPUT_POST, 'dn', FILTER_SANITIZE_STRING);
+      $genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_STRING);
+      $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+      $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+      $cp = filter_input(INPUT_POST, 'cp', FILTER_SANITIZE_STRING);
+      $ville = filter_input(INPUT_POST, 'ville', FILTER_SANITIZE_STRING);
+      $adr = filter_input(INPUT_POST, 'adr', FILTER_SANITIZE_STRING);
+      $comp = filter_input(INPUT_POST, 'comp', FILTER_SANITIZE_STRING);
+      $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_STRING);
+      $captcha = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+
+      // TRAITEMENT INSCIPTION POUR VALENTIN (C'est l'inscription au site)!
+
+    }
+  }
   if($responseKeys["success"]) {
     echo json_encode(array('success' => 'true'));
   } else {
     echo json_encode(array('success' => 'false'));
   }
 
+  if (isset($_POST["form_log"])) {
+    if (isset($_POST["mail"] && $_POST["mdp"]))
+    {
+      // METTRE LA VERIFICATION SI LES INFO DE LOG SONT BONNES
+    }
+  }
 
 ?>
