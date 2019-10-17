5296abcd5d93b2c741e0186694b7d5f74931a7b0
Nekodow <56070023+Nekodow@users.noreply.github.com>
2019-10-17 17:23:12 +0200

    correction bug conex

--- log.php
+++ log.php
@@ -11,8 +11,6 @@
         <form class="login-form" method="post" action="traitlog.php">
           <input type="text" placeholder="mail"/>
           <input type="password" placeholder="mot de passe"/>
-          <div class="g-recaptcha" style="position:relative; right:15px;" data-sitekey="6Lca1qQUAAAAADWm9IN2V9Em3Vj-CmPdo3LG0U3j"></div>
-          <br>
           <button>Connexion</button>
             <p class="message"> <a href="conex.html">Mot de passe oublié</a></p>
           <p class="message">Pas inscrit ? <a href="conex.html">Créer un compte</a></p>
