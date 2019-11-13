25843d69e30fe19eb89e4d015cb699b97efda469
Valentin <valentindevaud@gmail.com>
2019-11-13 11:46:35 +0100

    chgdj,ted

--- log.php
+++ log.php
@@ -1,31 +1,33 @@
 <?php
 include 'class/bdd.inc.php';
 $mailm = $_POST['mailm'];
+
 $mdpm = $_POST['mdpm'];
 
+
 //  Récupération de l'utilisateur et de son pass hashé
-$sql = "SELECT mailm, mdpm  FROM membre WHERE mailm = $mailm";
-$req=	$conn -> query($sql);
-$resultat = $req -> fetch();
-$mdpm = $resultat['mdpm'];
-echo $mdpm;
+$req = "SELECT mailm, mdpm FROM membre WHERE mailm = '$mailm' ";
+$res=	$conn -> query($req);
+$ligne = $res -> fetch();
+
+
 
 // Comparaison du pass envoyé via le formulaire avec la base
-//$isPasswordCorrect = password_verify($_POST['mdpm'], $resultat['mdpm']);
+$isPasswordCorrect = password_verify($mdpm, $ligne['mdpm']);
 
-if (!$result['mdpm'])
+if (!$ligne)
 {
     echo 'Mauvais identifiant ou mot de passe !';
 }
 else
 {
-    if ($_POST['mdpm']==$result['mdpm']) {
+    if ($mdpm == $ligne['mdpm']) {
         session_start();
-        $_SESSION['idmembre'] = $resultat['idmembre'];
+
         $_SESSION['mailm'] = $mailm;
         echo 'Vous êtes connecté !';
     }
     else {
-        echo 'Mauvais identifiant ou mot de passe ';
+        echo 'Mauvais identifiant ou mot de passe !!!';
     }
 }
