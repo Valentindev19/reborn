5f1a9b3bfa613ed1fde1413b1b045be14b438374
Valentin <valentindevaud@gmail.com>
2019-11-21 16:06:22 +0100

    art

--- traitarticle.php
+++ traitarticle.php
@@ -2,8 +2,9 @@
   include 'class/bdd.inc.php';
   include 'class/article.class.php';
 
-  if (isset($_POST['btn_ajche_form']))
+  if (isset($_POST['btn_ajart_form']))
   {
+
     if(isset($_POST['titre']) && isset($_POST['resume']) && isset($_POST['lien']) && isset($_POST['contenue']) && isset($_POST['img']))
     {
       $titre_article = $_POST['titre'];
@@ -14,6 +15,7 @@
 
       $article = new article("","","","","","");
       $article->ajoutart($titre_article, $resume_article, $lien_article, $contenue_article, $conn);
+
     }
     else
     {
@@ -22,16 +24,34 @@
   }
   if (isset($_GET['sup']))
   {
-    $id = $_GET['id'];
+    $idarticle = $_GET['id'];
     $article = new article("","","","","","");
     $article->suppart($idarticle,$conn);
+
+
+
   }
   if (isset($_GET['modif']))
   {
-
+    $idarticle = $_GET['id'];
     $article = new article("","","","","","");
-    $article->affarticle($idarticle, $conn);
+    $req = $article->affart($idarticle, $conn);
+    $ligne = $req->fetch();
+
+
     ?>
+    <html>
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="http:s//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <script language="JavaScript" src="https://code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
+    <script language="JavaScript" src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script language="JavaScript" src="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css">
+    <body>
     <div class="site-section bg-light">
       <div class="container">
         <div class="row">
@@ -40,57 +60,41 @@
 
 
 
-            <form method="post" action="traitadmin.php" id="modif_form" class="p-5 bg-white">
+            <form method="post" action="traitarticle.php" id="modif_form" class="p-5 bg-white">
 
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="font-weight-bold" for="race">idarticle</label>
-                  <input type="hidden" name="idche" id="idche" class="form-control" placeholder="Race du cheval" value="<?php echo $GET_['idarticle']; ?>">
+
+                  <input type="hidden" name="idart" id="idart" class="form-control" placeholder="Race du cheval" value="<?php echo $_GET['id']; ?>">
                 </div>
               </div>
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
                   <label class="font-weight-bold" for="race">Titre</label>
-                  <input type="text" name="titre" id="titre" class="form-control" placeholder="Race du cheval" value="<?php echo $ligne['titre_article']; ?>">
+                  <input type="text" name="titre" id="titre" class="form-control"  value="<?php echo $ligne['titre_article']; ?>">
                 </div>
               </div>
               <div class="row form-group">
                 <div class="col-md-12">
-                  <label class="font-weight-bold" for="nom">Nom cheval</label>
-                  <input type=text name="nom" id="nom" class="form-control" value="<?php echo $ligne['nomche']; ?>">
+                  <label class="font-weight-bold" for="nom">Résumé</label>
+                  <input type=text name="resume" id="resume" class="form-control" value="<?php echo $ligne['resume_article']; ?>">
                 </div>
               </div>
 
 
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="font-weight-bold" for="age">Age</label>
-                  <input type="number" name="age" id="age" class="form-control" value="<?php echo $ligne['ageche']; ?>">
+                  <label class="font-weight-bold" for="age">Lien</label>
+                  <input type="texte" name="lien" id="lien" class="form-control" value="<?php echo $ligne['lien_article']; ?>">
                 </div>
               </div>
 
               <div class="row form-group">
                 <div class="col-md-12">
-                  <label class="font-weight-bold" for="taille">Taille</label>
-                  <input name="taille" id="taille" class="form-control" value="<?php echo $ligne['tailleche']; ?>">
+                  <label class="font-weight-bold" for="taille">Contenue</label>
+                  <input name="contenue" id="contenue" class="form-control" value="<?php echo $ligne['contenue_article']; ?>">
                 </div>
               </div>
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="poids">Poids</label>
-                  <input name="poids" id="poids" class="form-control" value="<?php echo $ligne['poids']; ?>">
-                </div>
-              </div>
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="sexe">Sexe cheval</label>
-                  <input name="sexe" id="sexe" class="form-control" value="<?php echo $ligne['sexeche']; ?>">
-                </div>
-              </div>
-              <div class="row form-group">
-                <div class="col-md-12">
-                  <label class="font-weight-bold" for="img">Image</label>
-                  <input type="file" name="img" id="img" class="form-control" value="<?php echo $ligne['imageche']; ?>">
                 </div>
               </div>
 
@@ -106,18 +110,18 @@
     </div>
     <?php
     }
-    if (isset($_GET['btn_modif_form']) && isset($_POST['race']) && isset($_POST['nom']) && isset($_POST['age']) && isset($_POST['taille']) && isset($_POST['poids']) && isset($_POST['sexe']) &&
-    isset($_POST['img']))
+
+    if (isset($_POST['btn_modif_form']))
     {
-      $idche = $_POST['idche'];
-      $race = $_POST['race'];
-      $nom = $_POST['nom'];
-      $age = $_POST['age'];
-      $taille = $_POST['taille'];
-      $poids = $_POST['poids'];
-      $sexe = $_POST['sexe'];
-      $img = $_POST['img'];
-      $cheval->modifche($idche ,$race, $nomche, $ageche, $taille, $poids, $sexe, $image, $conn);
+      $idarticle = $_POST['idart'];
+      $titre_article = $_POST['titre'];
+      $resume_article = $_POST['resume'];
+      $lien_article = $_POST['lien'];
+      $contenue_article = $_POST['contenue'];
+      $article = new article("","","","","","");
+      $article->modifart($idarticle,$titre_article, $resume_article, $lien_article, $contenue_article, $conn);
+
+      header('Location:gestionart.php');
     }
 
 ?>
