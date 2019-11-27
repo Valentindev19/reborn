6529ade7bbb17822b4decb14e2d53ab721f66bde
axeljeannou <56070214+axeljeannou@users.noreply.github.com>
2019-11-27 14:59:37 +0100

    activité

---
+++ class/activite.class.php
@@ -0,0 +1,13 @@
+<?php
+
+  class activite
+  {
+    private $id;
+    private $;
+    private $;
+    private $;
+    private $taille;
+    private $poids;
+    private $sexe;
+    private $image;
+    private $conn;
