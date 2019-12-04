e269bd6bdc345ba1c3242017124e420182a82cc2
Valentin <valentindevaud@gmail.com>
2019-12-04 08:20:33 +0100

    cs

--- class/membre.class.php
+++ class/membre.class.php
@@ -97,7 +97,7 @@ class membre
   public function ajoutmembre($nom, $prenom, $sexe, $dn, $mailm, $phone, $adr, $comp, $mdp, $id_ville, $cle, $conn)
   {
     $req = "INSERT INTO membre(nomm,prenomm,genrem,ddn,mailm,telephonem,ruem,compm,mdpm,ville_id,id_typem,validemembre,cle)
-            VALUES('$nom','$prenom','$sexe',$dn,'$mailm','$phone','$adr','$comp','$mdp',$id_ville,2,1 /* il est mis en actif ici parceque l envoie de mail ne marche pas en local  */,'$cle');";
+            VALUES('$nom','$prenom','$sexe',$dn,'$mailm','$phone','$adr','$comp','$mdp',$id_ville,2,1 /* il est mis en actif ici parceque l envoie de mail ne marche pas en local  */,'$cle')";
     $conn -> Query($req);
     header("Location:membre.php");
   }
@@ -118,13 +118,7 @@ class membre
         $ligne = $res -> fetch();
         return $ligne;
       }
-      public function affmembre($mailm, $conn)
-      {
-        $req = "SELECT nomm, prenomm, genrem, ddn, mailm, telephonem, ruem, FROM membre WHERE mailm = '$mailm' ";
-        $res=	$conn -> query($req);
-        $ligne = $res -> fetch();
-        return $ligne;
-      }
+      
 
 }
 
