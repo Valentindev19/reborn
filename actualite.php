6d63e82acb66d498803694f9bf55423224d159cc
Nekodow <56070023+Nekodow@users.noreply.github.com>
2019-12-05 09:50:40 +0100

    continuité

--- actualite.php
+++ actualite.php
@@ -58,7 +58,7 @@
 
               <div class="col-2">
 
-                  <h2 class="mb-0 site-logo"><a href="index.html">Centre Equestre Saint Vitrac</a></h2>
+                  <h2 class="mb-0 site-logo"><a href="index.php">Centre Equestre Saint Vitrac</a></h2>
 
               </div>
               <div class="col-10">
@@ -67,17 +67,16 @@
                     <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
                     <ul class="site-menu js-clone-nav d-none d-lg-block">
-                      <li>
+                      <li class="active">
                         <a href="index.php">Accueil</a>
                       </li>
-                      <li class="active">
-                      <a href="actualite.php">Actualités</a></li>
+                      <li><a href="actualite.php">Actualités</a></li>
                       <li><a href="chevaux.php">Chevaux</a></li>
-                      <li><a href="pensions.php">Pensions</a></li>
+                      <li><a href="pensions.php">Pension</a></li>
                       <li class="has-children">
                         <a href="cours.php">Activités</a>
                         <ul class="dropdown arrow-top">
-                          <li><a href="cours.php">Cours</a></li>
+                          <li><a href="cours.php">Cour</a></li>
                           <li><a href="stage.php">Stage</a></li>
                           <li><a href="promenade.php">Promenade</a></li>
                           </li>
