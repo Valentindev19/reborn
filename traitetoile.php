f122f49c7e3ada4fc435bc37b9bed088908c50d4
Valentin <valentindevaud@gmail.com>
2019-12-11 09:54:47 +0100

    etoile

--- traitetoile.php
+++ traitetoile.php
@@ -12,6 +12,17 @@ $etoile = new etoile("","","","","");
 $req = $etoile->affid($conn,$mailm, $mdpm);
 $idmembre = $req->fetch();
 $idmembre = $idmembre['idmembre'];
+$aime = $etoile->vote($conn, $idche, $idmembre);
+$aime = $req->fetch();
+$aime = $aime['aime'];
+return $aime;
+if ($aime === NULL)
+{
 $etoile = new etoile("","","","","");
 $etoile->ajoutetoile($idche,$idmembre ,$etoi,$conn);
+}
+else
+{
+
+}
 ?>
