fbc9f8e9efa980b2d4c01bbee3aaa16bb05f6885
Valentin <valentindevaud@gmail.com>
2019-12-11 08:57:11 +0100

    qetzy

--- che.php
+++ che.php
@@ -168,6 +168,7 @@
                       <h2 class="h5 mb-3">Moyenne des membres :</h2>
                       <td><div id='0'><script type='text/javascript'>CreateListeEtoile('0',5);</script></div></td>
                       <td><div id='0'><script type='text/javascript'>GestionHover('0',<?php echo $aime['round(avg(aime))'];?>,5);</script></div></td>
+                      <td><div id='0'><script type='text/javascript'>ChoixSelection('0',<?php echo $aime['round(avg(aime))'];?>,5);</script></div></td>
 
 
 
