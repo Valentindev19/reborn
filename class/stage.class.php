eda22eb96da109ddc706764a38ed47a9ba571b41
Valentin <valentindevaud@gmail.com>
2019-12-12 10:37:39 +0100

    hh

---
+++ class/stage.class.php
@@ -0,0 +1,93 @@
+<?php
+//include'typestage.class.php';
+  class stage
+  {
+    private $idstage;
+    private $dated;
+    private $datef;
+    private $des;
+    private $heured;
+    private $heuref;
+    private $repas;
+    private $untype;
+    private $conn;
+
+
+    public function stage($idstage, $dated, $datef, $des, $heured, $heuref, $repas, $idtype_stage,$libtype_stage, $conn)
+    {
+      $this->idstage = $idstage;
+      $this->dated = $dated;
+      $this->datef  = $datef;
+      $this->des = $des;
+      $this->heured = $heured;
+      $this->heuref = $heuref;
+      $this->repas = $repas;
+    //  $type= new type_stage($idtype_stage,$libtype_stage);
+    //  $this->untype= $type;
+      $this->conn = $conn;
+    }
+////////////////////////////////////// Le GET
+    public function getid()
+    {
+      return $this->idstage;
+    }
+
+
+
+////////////////////////////////////////// AJOUT
+    public function ajoutstage($idtype_stage, $dated, $datef, $des, $heured, $heuref, $repas, $conn)
+    {
+      $SQL = "INSERT INTO stage (idtype_stage, dated, datef, des, heured, heured, repas, validestage)
+              VALUES ('$idtype_stage', '$dated', $datef, $des, $heured, '$heuref', '$repas', 1)";
+      $conn->query($SQL);
+      header('Location:gestionsta.php');
+    }
+////////////////////////////////////////// SUPPRIMER
+    public function suppstage($idstage, $conn)
+    {
+      $SQL = "UPDATE stage
+              SET validestage = 0
+              WHERE idstage ='$idstage'";
+      $conn -> query($SQL);
+      header("Location:gestionsta.php");
+    }
+////////////////////////////////////////// MODIFIER
+    public function modifche($idche ,$race, $nomche, $ageche, $taille, $poids, $sexe, $image, $conn)
+    {
+      $SQL = "UPDATE cheval
+              SET idrace = '$race', nomche = '$nomche', ageche = $ageche, tailleche = $taille, poids = $poids, sexeche = '$sexe', imageche = '$image'
+              WHERE idche =$idche";
+      $conn -> query($SQL);
+      header("Location:gestionche.php");
+    }
+////////////////////////////////////////// AFFICHER
+    public function affche($idche, $conn)
+    {
+      $SQL = "SELECT race.librace, nomche, ageche, tailleche, poids, sexeche, imageche
+      FROM cheval,race
+      WHERE cheval.idrace = race.idrace
+      AND idche ='$idche'";
+
+      $res = $conn -> query($SQL);
+      return $res;
+    }
+
+    public function affstage($conn)
+    {
+      $SQL = "SELECT idstage,type_stage.typestage, datedstage, datefstage, descstage, heuredstage, heurefstage, repas
+      FROM stage,type_stage
+      WHERE stage.idtype_stage = type_stage.idtype_stage
+      AND validestage= 1";
+      $req = $conn -> query($SQL);
+      return $req;
+    }
+    public function afftype($conn)
+    {
+      $SQL = "SELECT idtype_stage, typestage
+      FROM type_stage";
+      $req = $conn -> query($SQL);
+      return $req;
+    }
+}
+
+?>
---
+++ gestionsta.php
@@ -0,0 +1,206 @@
+<?php
+// On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
+session_start ();
+include 'class/stage.class.php';
+include 'class/bdd.inc.php';
+// On récupère nos variables de session
+if (isset($_SESSION['mailm']) && isset($_SESSION['mdpm']) && isset($_SESSION['id_typem'])) {
+?>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<script language="JavaScript" src="https://code.jquery.com/jquery-1.11.1.min.js" type="text/javascript"></script>
+<script language="JavaScript" src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script language="JavaScript" src="https://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function() {
+$('#datatable').dataTable(
+	{
+		"language": {
+				"url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
+		}
+	}
+);
+
+ $("[data-toggle=tooltip]").tooltip();
+
+} );
+</script>
+
+<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/plug-ins/3cfcc339e89/integration/bootstrap/3/dataTables.bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/creative.css">
+<body>
+<div class="container">
+	<div class="row">
+		<h2 class="text-center">Gestion Stages</h2>
+	</div>
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+
+<table id="datatable" class="table table-striped table-bordered" cellspacing="0" width="100%">
+    				<thead>
+							<tr>
+								<th>Type Stage</th>
+								<th>Date Début</th>
+								<th>Heure Début</th>
+								<th>Description</th>
+								<th>Heure Début</th>
+								<th>Heure Fin</th>
+								<th>Repas</th>
+	                                <th>Modifier</th>
+	                                 <th>Supprimer</th>
+							</tr>
+					</thead>
+					<?php
+
+
+$stage = new stage("","","","","","","","","","");
+$req = $stage->affstage($conn);
+//$res = $untype->afftypestage($conn);
+?>
+					<tfoot>
+            <tr>
+							<th>Type Stage</th>
+							<th>Date Début</th>
+							<th>Heure Début</th>
+							<th>Description</th>
+							<th>Heure Début</th>
+							<th>Heure Fin</th>
+							<th>Repas</th>
+																<th>Modifier</th>
+																 <th>Supprimer</th>
+						</tr>
+					</tfoot>
+
+					<tbody>
+						<?php
+	            while ($ligne = $req -> fetch())
+	            {
+  echo"<tr>";
+    echo"<td>",$ligne['typestage'],"</td>";
+		echo"<td>",$ligne['datedstage'],"</td>";
+		echo"<td>",$ligne['datefstage'],"</td>";
+    echo"<td>",$ligne['descstage'],"</td>";
+		echo"<td>",$ligne['heuredstage'],"</td>";
+		echo"<td>",$ligne['heurefstage'],"</td>";
+    echo"<td>",$ligne['repas'],"</td>";
+    echo"<td> <a href='traitstage.php?mod=mod&id=$ligne[idstage]'><img src='images/modadmin.png' alt='edit' title='modifier' width=35 /></a></td>";
+    echo"<td> <a href='traitstage.php?sup=sup&id=$ligne[idstage]'><img src='images/supadmin.png' alt='supprimerche' title='Supprimer' width=20 /></a></td>";
+  echo"</tr>";
+}
+echo"</center>";
+
+?>
+
+					</tbody>
+				</table>
+				<a href='admin.php'><button class="boutonret">Retour</button></a>
+
+
+	</div>
+	</div>
+</div>
+<br>
+<br>
+  <div class="site-section bg-light">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12 col-lg-8 mb-5">
+
+
+
+          <form method="post" action="traitche.php" id="contact_form" class="p-5 bg-white" enctype="multipart/form-data">
+
+            <div class="row form-group">
+              <div class="col-md-12 mb-3 mb-md-0">
+                <label class="font-weight-bold" for="race">Race</label>
+								<tr>
+				<td>
+				<select name="idrace">
+			<?php
+					while ($l = $res -> fetch())
+					{
+						?>
+							<option value="<?php echo $l['idrace']?>"><?php echo $l['librace']?></option>
+					 <?php
+					}
+					?>
+				</select>
+				</td>
+			</tr>
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-md-12">
+                <label class="font-weight-bold" for="nom">Nom cheval</label>
+                <input type=text name="nom" id="nom" class="form-control" placeholder="Nom du cheval">
+              </div>
+            </div>
+
+
+            <div class="row form-group">
+              <div class="col-md-12 mb-3 mb-md-0">
+                <label class="font-weight-bold" for="age">Age</label>
+                <input type="number" name="age" id="age" class="form-control" placeholder="Age du cheval">
+              </div>
+            </div>
+
+            <div class="row form-group">
+              <div class="col-md-12">
+                <label class="font-weight-bold" for="taille">Taille</label>
+                <input type="number" name="taille" id="taille" class="form-control" placeholder="Taille du cheval en cm">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-md-12">
+                <label class="font-weight-bold" for="poids">Poids</label>
+                <input type="number" name="poids" id="poids" class="form-control" placeholder="Poids du cheval en Kg">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-md-12">
+                <label class="font-weight-bold" for="sexe">Sexe cheval</label>
+                <input name="sexe" id="sexe" class="form-control" placeholder="Sexe du cheval">
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-md-12">
+                <label class="font-weight-bold" for="img">Image</label>
+                <input type="file" name="fileToUpload" id="fileToUpload" class="form-control">
+              </div>
+            </div>
+
+            <div class="row form-group">
+              <div class="col-md-12">
+                <td><input type ="submit" name="btn_ajche_form" value="Envoyer" class="btn btn-primary pill px-4 py-2">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+		<script>
+		$(document).ready(function() {
+    $('#datatable').dataTable();
+
+     $("[data-toggle=tooltip]").tooltip();
+
+} );
+    </script>
+    </body>
+		<?php
+		}
+		else {
+			header ('location: log.php');
+
+		}
+		?>
+		</html>
